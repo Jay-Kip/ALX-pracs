375aa95f93f0e5544fe562311cf5376e9e0b9b72
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-05-31 10:02:08 +0000

    Static libraries practice

---
+++ char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()
+{
+	char a;
+
+	for (a >= 'a'; a <= 'z'; a++)
+	{
+		printf("%c", a);
+	}
+	return (0);
+}
---
+++ sub.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+
+void sub(int a, int b)
+{
+	printf("%d\n", a - b);
+}
