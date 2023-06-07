22a9d16dd8393a6ba20c8f5d52607f0e5b8f9987
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-06-07 19:54:59 +0000

    Text operations

---
+++ scripts/hello.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cat <<< did >> Hello.txt
+
+#command that accepts all texts written on the CLI until the user finally types "did" that it appends the texts written to the Hello.txt file
