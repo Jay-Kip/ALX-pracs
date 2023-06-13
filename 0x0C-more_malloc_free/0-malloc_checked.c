5864fb4b65d72aa9e736cdbb116d0878f9ed2eff
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-06-13 12:47:25 +0000

    function

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return (p);
+
+}
