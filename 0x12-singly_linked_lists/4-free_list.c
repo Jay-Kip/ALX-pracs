1198cabe0aab7d066ce0fea4b07de94080e30333
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-07-05 18:28:13 +0000

    a

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+
+}
